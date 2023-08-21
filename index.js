df065cd7d9efc2c002b7a760ebcb90c9143306d8
Roniclay <roniclayrodrigues07@gmail.com>
2023-08-21 20:28:12 -0300

    Atualizando swagger

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const swaggerUi =  require('swagger-ui-express')
+const swaggerDocs = require('./swagger.json')
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+
+app.use(express.json())
 app.use(bodyParser.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 const users = [];
 
 // POST- criando novo usuário
+
 app.post('/users', (req, res) => {
   const { cpf, nome, data_nascimento } = req.body;
   const newUser = { cpf, nome, data_nascimento };
@@ -16,6 +22,8 @@ app.post('/users', (req, res) => {
 });
 
 // GET- obtendo usuários
+
+
 app.get('/users', (req, res) => {
   res.json(users);
 });
@@ -28,4 +36,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta http://localhost:${port}/ \nDados nahttp://localhost:${port}/users `);
 });
+
 
